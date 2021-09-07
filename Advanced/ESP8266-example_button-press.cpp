a4a304be77a5e98b12d0ff31c1c8df4bf12e1e52
StorageB <44729718+StorageB@users.noreply.github.com>
2021-09-07 17:13:24 -0500

    Create ESP8266-example_button-press.cpp

---
+++ Advanced/ESP8266-example_button-press.cpp
@@ -0,0 +1,150 @@
+// Example Arduino/ESP8266 code to upload data to Google Sheets when a button is pressed
+// Follow setup instructions found here:
+// https://github.com/StorageB/Google-Sheets-Logging
+// reddit: u/StorageB107
+// email: StorageUnitB@gmail.com
+//
+// This example uses the HTTPSRedirect library by Sujay Phadke
+// https://github.com/electronicsguy/ESP8266
+
+
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include "HTTPSRedirect.h"
+
+// Enter network credentials:
+const char* ssid     = "network";
+const char* password = "password";
+
+// Enter Google Script Deployment ID:
+const char *GScriptId = "AKfycpwCtWuN3BNIO5CR9BtZJAMaRNUgusldcAz6V7rtGCg1L5srZ5iAEZumVeb-1ea5Y6mg3Q";
+
+// Enter command (insert_row or append_row) and your Google Sheets sheet name (default is Sheet1):
+String payload_base =  "{\"command\": \"insert_row\", \"sheet_name\": \"Sheet1\", \"values\": ";
+String payload = "";
+
+// Google Sheets setup (do not edit)
+const char* host = "script.google.com";
+const int httpsPort = 443;
+const char* fingerprint = "";
+String url = String("/macros/s/") + GScriptId + "/exec";
+HTTPSRedirect* client = nullptr;
+
+// Declare variables that will be published to Google Sheets
+int value0 = 0;
+int value1 = 0;
+int value2 = 0;
+
+// Declare variables that will keep track of whether or not the data has been published
+bool data_published = false;
+int error_count = 0;
+
+void setup() {
+
+  Serial.begin(9600);        
+  delay(10);
+  Serial.println('\n');
+  
+  // Declare pin D1 as a button input:
+  pinMode(D1, INPUT);
+  
+  // Connect to WiFi
+  WiFi.begin(ssid, password);             
+  Serial.print("Connecting to ");
+  Serial.print(ssid); Serial.println(" ...");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+  }
+  Serial.println('\n');
+  Serial.println("Connection established!");  
+  Serial.print("IP address:\t");
+  Serial.println(WiFi.localIP());
+
+  // Use HTTPSRedirect class to create a new TLS connection
+  client = new HTTPSRedirect(httpsPort);
+  client->setInsecure();
+  client->setPrintResponseBody(true);
+  client->setContentTypeHeader("application/json");
+  
+  Serial.print("Connecting to ");
+  Serial.println(host);
+
+  // Try to connect for a maximum of 5 times
+  bool flag = false;
+  for (int i=0; i<5; i++){ 
+    int retval = client->connect(host, httpsPort);
+    if (retval == 1){
+       flag = true;
+       Serial.println("Connected");
+       break;
+    }
+    else
+      Serial.println("Connection failed. Retrying...");
+  }
+  if (!flag){
+    Serial.print("Could not connect to server: ");
+    Serial.println(host);
+    return;
+  }
+  delete client;    // delete HTTPSRedirect object
+  client = nullptr; // delete HTTPSRedirect object
+}
+
+
+void loop() {
+
+  // attempt to publish to Google Sheets when button is pressed (when pin reads HIGH)
+  if (digitalRead(D1)== HIGH)  {
+
+    // before attempting to publish to Google Sheets, set the data_published variable to false
+    data_published = false;
+    error_count = 0;
+    
+    // create some fake data to publish
+    value0 ++;
+    value1 = random(0,1000);
+    value2 = random(0,100000);
+
+    // this while loop will attempt to publish data up to 3 times
+    while(data_published == false && error_count < 3){
+
+      static bool flag = false;
+      if (!flag){
+        client = new HTTPSRedirect(httpsPort);
+        client->setInsecure();
+        flag = true;
+        client->setPrintResponseBody(true);
+        client->setContentTypeHeader("application/json");
+      }
+      if (client != nullptr){
+        if (!client->connected()){
+          client->connect(host, httpsPort);
+        }
+      }
+      else{
+        Serial.println("Error creating client object!");
+      }
+
+      // Create json object string to send to Google Sheets
+      payload = payload_base + "\"" + value0 + "," + value1 + "," + value2 + "\"}";
+  
+      // Publish data to Google Sheets
+      Serial.println("Publishing data...");
+      Serial.println(payload);
+    
+      if(client->POST(url, host, payload)){ 
+        // do stuff here if publish was successful
+        data_published = true;
+      }
+      else{
+       // do stuff here if publish was not successful
+       Serial.println("Error while connecting");
+       error_count++;
+       delay(2000);
+      }
+      yield();
+    } 
+  }
+}
